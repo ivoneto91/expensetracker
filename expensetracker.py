6dca65d15ff3643c2330dcf629ba1ce71c6a4641
ivoneto91 <67297669+ivoneto91@users.noreply.github.com>
2022-12-13 03:05:07 -0300

    improving menu and some date interactions

--- expensetracker.py
+++ expensetracker.py
@@ -1,6 +1,7 @@
 import os # os.system("cls")
+from datetime import date
 
-class Tracker:
+class Tracker: #where we will store the data
     def __init__(self):
         self.name = []
         self.date = []
@@ -8,7 +9,7 @@ class Tracker:
         self.installments = []
         self.category = []
 
-class Expense:
+class Expense: #where we will create the data
     def __init__(self,name,date,value,installments,category):
         self.name = str(name) 
         self.date = date
@@ -25,6 +26,7 @@ def startingmenu():
 
 #This is the menu with the expenses type
 def categoryexpenses():
+    print("    Creating a new expense    \n")
     print("Select 1 for Household expenses.")
     print("Select 2 for Food expenses.")
     print("Select 3 for Transport expenses.")
@@ -32,37 +34,110 @@ def categoryexpenses():
     print("Select 5 for Health expenses.")
     print("Select 6 for Miscellaneous expenses.")
 
-
-
 startingmenu()
 menuans = int(input())
-while menuans != 1 and menuans != 2 and menuans != 3:
+while menuans not in (1, 2, 3):
     print("Invalid Option. Please select 1, 2 or 3.")
     menuans = int(input())
-if menuans == 1:
-    os.system("cls")
-    categoryexpenses()
-    categoryans = int(input())
-    while categoryans != 1 and categoryans != 2 and categoryans != 3 and categoryans != 4 and categoryans != 5 and categoryans != 6:
-        print("Invalid Option. Press 1, 2, 3, 4, 5 or 6 to create a new expense.")
+else:
+    if menuans == 1:
+        os.system("cls")
+        categoryexpenses()
         categoryans = int(input())
-        if categoryans == 1:
-            print("Adding a new Household expense.")
-        elif categoryans == 2:
-            print("Adding a new Food expense.")
-        elif categoryans == 3:
-            print("Adding a new Transport expense.")
-        elif categoryans == 4:
-            print("Adding a new Leisure expense.")
-        elif categoryans == 5:
-            print("Adding a new Health expense.")
-        elif categoryans == 6:
-            print("Adding a new Miscellaneous expense.")         
-elif menuans == 2:
-    print("ss2")
-elif menuans == 3:
-    print("Thanks for using. See you next time : )")
-    exit()
+        while categoryans not in (1, 2, 3, 4, 5, 6,):
+            print("Invalid Option. Press 1, 2, 3, 4, 5 or 6 to create a new expense.")
+            categoryans = int(input())
+        else:
+            if categoryans == 1:
+                os.system("cls")
+                print("Adding a new Household expense.")
+                date = input("Did you made this expense today?(Y/N): ")
+                while date.lower() not in ("y", "yes", "n", "no"):
+                    print("Wrong Input. Select Y or N.")
+                    date = input("Did you made this expense today?(Y/N): ")
+                else:
+                    if date.lower() in ("y", "yes"):
+                        #we will import date from module.
+                        print("yfortest")
+                    elif date.lower() in ("n", "no"):
+                        #we will ask for user data
+                        print("nfortest")
+            elif categoryans == 2:
+                os.system("cls")
+                print("Adding a new Food expense.")
+                date = input("Did you made this expense today?(Y/N): ")
+                while date.lower() not in ("y", "yes", "n", "no"):
+                    print("Wrong Input. Select Y or N.")
+                    date = input("Did you made this expense today?(Y/N): ")
+                else:
+                    if date.lower() in ("y", "yes"):
+                        #we will import date from module.
+                        print("yfortest")
+                    elif date.lower() in ("n", "no"):
+                        #we will ask for user data
+                        print("nfortest")
+            elif categoryans == 3:
+                os.system("cls")
+                print("Adding a new Transport expense.")
+                date = input("Did you made this expense today?(Y/N): ")
+                while date.lower() not in ("y", "yes", "n", "no"):
+                    print("Wrong Input. Select Y or N.")
+                    date = input("Did you made this expense today?(Y/N): ")
+                else:
+                    if date.lower() in ("y", "yes"):
+                        #we will import date from module.
+                        print("yfortest")
+                    elif date.lower() in ("n", "no"):
+                        #we will ask for user data
+                        print("nfortest")
+            elif categoryans == 4:
+                os.system("cls")
+                print("Adding a new Leisure expense.")
+                date = input("Did you made this expense today?(Y/N): ")
+                while date.lower() not in ("y", "yes", "n", "no"):
+                    print("Wrong Input. Select Y or N.")
+                    date = input("Did you made this expense today?(Y/N): ")
+                else:
+                    if date.lower() in ("y", "yes"):
+                        #we will import date from module.
+                        print("yfortest")
+                    elif date.lower() in ("n", "no"):
+                        #we will ask for user data
+                        print("nfortest")
+            elif categoryans == 5:
+                os.system("cls")
+                print("Adding a new Health expense.")
+                date = input("Did you made this expense today?(Y/N): ")
+                while date.lower() not in ("y", "yes", "n", "no"):
+                    print("Wrong Input. Select Y or N.")
+                    date = input("Did you made this expense today?(Y/N): ")
+                else:
+                    if date.lower() in ("y", "yes"):
+                        #we will import date from module.
+                        print("yfortest")
+                    elif date.lower() in ("n", "no"):
+                        #we will ask for user data
+                        print("nfortest")
+            elif categoryans == 6:
+                os.system("cls")
+                print("Adding a new Miscellaneous expense.")
+                date = input("Did you made this expense today?(Y/N): ")
+                while date.lower() not in ("y", "yes", "n", "no"):
+                    print("Wrong Input. Select Y or N.")
+                    date = input("Did you made this expense today?(Y/N): ")
+                else:
+                    if date.lower() in ("y", "yes"):
+                        #we will import date from module.
+                        print("yfortest")
+                    elif date.lower() in ("n", "no"):
+                        #we will ask for user data
+                        print("nfortest")         
+    elif menuans == 2:
+        #here we will generate reports
+        print("ss2")
+    elif menuans == 3:
+        print("Thanks for using. See you next time : )")
+        exit()
 
 
 
